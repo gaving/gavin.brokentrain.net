1df40b38429639f3269048a28fca0b3e52e6907f
gavin <gavin@11554478-1e1d-0410-9119-d7a2b7ab7ba2>
2006-09-11 23:43:06 +0000

    Settling in changes, reimport feedparser since can't install it properly on server. Added list of svn items, changed getatime to getctime.
    git-svn-id: http://svn.brokentrain.net/gavbt/trunk@4 11554478-1e1d-0410-9119-d7a2b7ab7ba2

--- main/views.py
+++ main/views.py
@@ -1,59 +1,68 @@
 import os.path
-import feedparser
+from feedparser import feedparser
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
-	""" Entry point for the root of the site """
+    """ Entry point for the root of the site """
 
-	index = {
-			'SVN' : {
-					'name' : 'svn',
-					'url' : 'http://svn.brokentrain.net/',
-					'items' : 5
-				},
-			'PROJECTS' : {
-					'name' : 'projects',
-					'url' : 'projects/',
-					'items' : 5
-				},
-			'LAST_FM' : {
-					'name' : 'music',
-					'url' : 'http://www.last.fm/user/gaving',
-					'items' : 8,
-					'feed' : 'http://ws.audioscrobbler.com/1.0/user/gaving/recenttracks.rss'
-				},
-			'UPLOAD' : {
-					'name' : 'upload',
-					'url' : 'upload/',
-					'items' : 10
-				}
-			}
+    index = {
+            'SVN' : {
+                    'name' : 'svn',
+                    'url' : 'http://svn.brokentrain.net/',
+                    'repodir' : '../svn',
+                    'items' : 5
+                },
+            'PROJECTS' : {
+                    'name' : 'projects',
+                    'url' : 'projects/',
+                    'items' : 5
+                },
+            'LAST_FM' : {
+                    'name' : 'music',
+                    'url' : 'http://www.last.fm/user/gaving',
+                    'items' : 8,
+                    'feed' : 'http://ws.audioscrobbler.com/1.0/user/gaving/recenttracks.rss'
+                },
+            'UPLOAD' : {
+                    'name' : 'upload',
+                    'url' : 'upload/',
+                    'items' : 10
+                }
+            }
 
-	projects = []
-	for file in os.listdir(index['PROJECTS']['url']):
-		fullpath = os.path.join(index['PROJECTS']['url'], file)
-		if os.path.isdir(fullpath):
-			projects.append((os.path.getatime(fullpath), file, fullpath))
+    repos = []
+    for file in os.listdir(index['SVN']['repodir']):
+        fullpath = os.path.join(index['SVN']['repodir'], file)
+        if os.path.isdir(fullpath):
+            repos.append(file)
+    repos.sort()
 
-	projects.sort()
-	projects.reverse()
+    projects = []
+    for file in os.listdir(index['PROJECTS']['url']):
+        fullpath = os.path.join(index['PROJECTS']['url'], file)
+        if os.path.isdir(fullpath):
+            projects.append((os.path.getctime(fullpath), file, fullpath))
 
-	uploads = []
-	for file in os.listdir(index['UPLOAD']['url']):
-		fullpath = os.path.join(index['UPLOAD']['url'], file)
-		uploads.append((os.path.getatime(fullpath), file, fullpath))
+    projects.sort()
+    projects.reverse()
 
-	uploads.sort()
-	uploads.reverse()
+    uploads = []
+    for file in os.listdir(index['UPLOAD']['url']):
+        fullpath = os.path.join(index['UPLOAD']['url'], file)
+        uploads.append((os.path.getatime(fullpath), file, fullpath))
 
-	d = feedparser.parse(index['LAST_FM']['feed'])
+    uploads.sort()
+    uploads.reverse()
 
-	return render_to_response('main/index.html', {
-		'index': index,
-		'projects' : projects[:index['PROJECTS']['items']],
-		'tracks' : d.entries[:index['LAST_FM']['items']],
-		'uploads' : uploads[:index['UPLOAD']['items']]
-	})
+    d = feedparser.parse(index['LAST_FM']['feed'])
 
-# vim: set expandtab shiftwidth=4 softtabstop=4 textwidth=79:
+    return render_to_response('main/index.html', {
+            'index': index,
+            'svn' : repos,
+            'projects' : projects[:index['PROJECTS']['items']],
+            'tracks' : d.entries[:index['LAST_FM']['items']],
+            'uploads' : uploads[:index['UPLOAD']['items']]
+            })
+
+    # vim: set expandtab shiftwidth=4 softtabstop=4 textwidth=79:
--- settings.py
+++ settings.py
@@ -67,7 +67,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/mnt/media/httpd/projects/django/gavbt/templates'
+	'/home/gav/django_projects/gavbt/templates'
 )
 
 INSTALLED_APPS = (

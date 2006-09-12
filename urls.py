f7f7d317981fa9f405b6f9f1c5a891b66aca0e4a
gavin <gavin@11554478-1e1d-0410-9119-d7a2b7ab7ba2>
2006-09-12 00:29:50 +0000

    Removed development urls, added appropriate apache rewrite ones.
    git-svn-id: http://svn.brokentrain.net/gavbt/trunk@5 11554478-1e1d-0410-9119-d7a2b7ab7ba2

--- urls.py
+++ urls.py
@@ -9,11 +9,4 @@ urlpatterns = patterns('',
      
 	 # Main index
 	 (r'^$', 'gavbt.main.views.index'),
-
-	 # File directories
-	 (r'^projects/(?P<path>.*)$', 'django.views.static.serve', 
-		 {'document_root' : 'projects/', 'show_indexes': True}),
-
-	 (r'^upload/(?P<path>.*)$', 'django.views.static.serve', 
-		 {'document_root' : 'upload/', 'show_indexes': True})
 )
